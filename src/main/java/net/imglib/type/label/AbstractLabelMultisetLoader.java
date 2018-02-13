6ef8d6dc48e694964901fabd6b8e094c1eaa402a
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-02-13 16:48:50 -0500

    Extract LabelMultisetType N5CacheLoader from BigCAT
    Classes were copied from the n5cacheloader branch of @shrucis1
    https://github.com/shrucis1/bigcat/tree/n5cacheloader
    The classes that were copied in this commit were created by @shrucis1.

---
+++ src/main/java/net/imglib/type/label/AbstractLabelMultisetLoader.java
@@ -0,0 +1,67 @@
+package net.imglib.type.label;
+
+import java.nio.ByteBuffer;
+
+import net.imglib2.cache.CacheLoader;
+import net.imglib2.img.cell.Cell;
+import net.imglib2.img.cell.CellGrid;
+import net.imglib2.util.Intervals;
+
+/**
+ * A type of {@link CacheLoader} that loads a {@link Cell} of
+ * {@link VolatileLabelMultisetArray} from a {@link Long} index and an
+ * underlying {@code byte[]}, whose source is determined by subclasses
+ *
+ * @author Neil Thistlethwaite
+ */
+
+// TODO this currently only works because it resides in the same package as LongMappedAccessData (albeit in a different artifact).
+// This is very fragile!
+
+public abstract class AbstractLabelMultisetLoader implements CacheLoader< Long, Cell< VolatileLabelMultisetArray > >
+{
+
+	protected final CellGrid grid;
+
+	public AbstractLabelMultisetLoader( final CellGrid grid )
+	{
+		this.grid = grid;
+	}
+
+	protected abstract byte[] getData( long... gridPosition );
+
+	@Override
+	public Cell< VolatileLabelMultisetArray > get( final Long key )
+	{
+
+		final int numDimensions = grid.numDimensions();
+
+		final long[] cellMin = new long[ numDimensions ];
+		final int[] cellSize = new int[ numDimensions ];
+		final long[] gridPosition = new long[ numDimensions ];
+		final int[] cellDimensions = new int[ numDimensions ];
+
+		grid.cellDimensions( cellDimensions );
+
+		grid.getCellDimensions( key, cellMin, cellSize );
+
+		for ( int i = 0; i < numDimensions; ++i )
+			gridPosition[ i ] = cellMin[ i ] / cellDimensions[ i ];
+
+		final byte[] bytes = this.getData( gridPosition );
+
+		final ByteBuffer bb = ByteBuffer.wrap( bytes );
+
+		final int[] data = new int[ ( int ) Intervals.numElements( cellSize ) ];
+		final int listDataSize = bytes.length - 4 * data.length;
+		final LongMappedAccessData listData = LongMappedAccessData.factory.createStorage( listDataSize );
+
+		for ( int i = 0; i < data.length; ++i )
+			data[ i ] = bb.getInt();
+
+		for ( int i = 0; i < listDataSize; ++i )
+			ByteUtils.putByte( bb.get(), listData.data, i );
+
+		return new Cell<>( cellSize, cellMin, new VolatileLabelMultisetArray( data, listData, true ) );
+	}
+}
---
+++ src/main/java/net/imglib/type/label/N5CacheLoader.java
@@ -0,0 +1,48 @@
+package net.imglib.type.label;
+
+import java.io.IOException;
+
+import org.janelia.saalfeldlab.n5.DataBlock;
+import org.janelia.saalfeldlab.n5.DatasetAttributes;
+import org.janelia.saalfeldlab.n5.N5Reader;
+
+import net.imglib2.img.cell.CellGrid;
+
+public class N5CacheLoader extends AbstractLabelMultisetLoader
+{
+	private final N5Reader n5;
+
+	private final String dataset;
+
+	public N5CacheLoader( final N5Reader n5, final String dataset ) throws IOException
+	{
+		super( generateCellGrid( n5, dataset ) );
+		this.n5 = n5;
+		this.dataset = dataset;
+	}
+
+	private static CellGrid generateCellGrid( final N5Reader n5, final String dataset ) throws IOException
+	{
+		final DatasetAttributes attributes = n5.getDatasetAttributes( dataset );
+
+		final long[] dimensions = attributes.getDimensions();
+		final int[] cellDimensions = attributes.getBlockSize();
+
+		return new CellGrid( dimensions, cellDimensions );
+	}
+
+	@Override
+	protected byte[] getData( final long... gridPosition )
+	{
+		final DataBlock< ? > block;
+		try
+		{
+			block = n5.readBlock( dataset, n5.getDatasetAttributes( dataset ), gridPosition );
+		}
+		catch ( final IOException e )
+		{
+			throw new RuntimeException( e );
+		}
+		return ( byte[] ) block.getData();
+	}
+}
