a9941e3a529f6d486d80813b2998e8abb298037e
Caleb Hulbert <cmhulbert@gmail.com>
2024-05-03 14:13:54 -0400

    chore: remove unused imports

--- src/test/java/net/imglib2/type/label/LabelMultisetEntryListTest.java
+++ src/test/java/net/imglib2/type/label/LabelMultisetEntryListTest.java
@@ -1,16 +1,6 @@
 package net.imglib2.type.label;
 
-import com.google.common.collect.Streams;
-import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Random;
-import java.util.Set;
-import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
 
